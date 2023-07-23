a1a92004d17cec11df66c2e9987de75a7bc6313c
heiberik <heiberik@hotmail.com>
2023-07-23 15:01:10 +0200

    added vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Link from 'next/link'
 import ThemeProvider from "@/components/themeProvider"
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 const settings = await getSettings()
@@ -19,13 +21,13 @@ export default function RootLayout({ children }) {
         <html lang="no">
             <body className={`${inter.className} min-h-screen relative  flex flex-col`}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-
                     <Header>
                         {routes.map((route) => {
                             return <Link key={route.url} href={route.url} className={`hover:opacity-80 p-3 text-center whitespace-nowrap ${route.colorLink ? "bg-main-color text-main-color rounded md:ml-3 hover:opacity-80 2xl:px-6" : "2xl:px-4"}`}>{route.title}</Link>
                         })}
                     </Header>
                     {children}
+                    <Analytics />
                 </ThemeProvider>
             </body>
         </html>
